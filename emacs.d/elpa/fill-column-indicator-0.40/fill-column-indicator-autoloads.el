f8614e944c4b048871ecb2f71bc2d158050b7f3e
Bryan Berry <bryan.berry@gmail.com>
2011-11-12 22:35:15 +0100

    added tweaks from mattrays .emacs

---
+++ emacs.d/elpa/fill-column-indicator-0.40/fill-column-indicator-autoloads.el
@@ -0,0 +1,18 @@
+;;; fill-column-indicator-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("fill-column-indicator-pkg.el" "fill-column-indicator.el")
+;;;;;;  (20158 58505 600651))
+
+;;;***
+
+(provide 'fill-column-indicator-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; fill-column-indicator-autoloads.el ends here
---
+++ emacs.d/elpa/fill-column-indicator-0.40/fill-column-indicator-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fill-column-indicator" "0.40" "graphically indicate the fill column" (quote nil))
