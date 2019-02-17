582b25c9e9c568560cacdc3458a11a55ddcf3493
hitman <hitman@localhost.localdomain>
2019-02-18 06:48:29 +0700

    Use ccls instead of irony-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -636,7 +636,7 @@ _s_: bash strict mode
  '(imenu-anywhere-buffer-filter-functions (quote (imenu-anywhere-same-project-p)))
  '(package-selected-packages
    (quote
-    (counsel-gtags company-gtags flymake-json smart-mode-line-powerline-theme js2-mode irony-eldoc flycheck-irony company-irony irony lsp-ui company-lsp lsp-mode lsp-javascript-typescript magit dockerfile-mode rjsx-mode rjsx indium js-comint helm-dash flymake-solidity solidity-mode go-mode projectile ivy buffer-move dracula-theme pyvenv ace-window atom-one-dark atom-dark-theme atom-one-dark-theme nov imenu-anywhere counsel-dash rubocop mmm-jinja2 groovy-mode company meghanada ivy-gitlab gitlab mvn dired-quick-sort hydra dired+ lorem-ipsum hc-zenburn-theme zenburn-theme swiper all-the-icons-ivy org org-brain undo-tree avy f s beacon vhdl-tools company-c-headers web-mode company-tern tern-auto-complete nodejs-repl tern mmm-mode better-shell py-autopep8 intero toml-mode haskell-mode ac-haskell-process tide dired-hacks-utils yaml-mode use-package typescript tablist sudo-edit spinner solarized-theme seq restclient queue powershell pdf-tools ox-pandoc org-present org-mobile-sync multi-eshell markdown-mode magit-gh-pulls know-your-http-well key-seq json-mode jinja2-mode ivy-hydra inf-ruby ido-vertical-mode helm-projectile helm-org-rifle helm-mt helm-gitlab helm-ag hcl-mode gradle-mode golint go-eldoc go-autocomplete gist ggtags flycheck flx-ido exec-path-from-shell eshell-manual elpy dumb-jump counsel-projectile clojure-mode cl-lib-highlight ansible-doc ag)))
+    (dash dash-at-point ccls counsel-gtags company-gtags flymake-json smart-mode-line-powerline-theme js2-mode irony-eldoc flycheck-irony company-irony irony lsp-ui company-lsp lsp-javascript-typescript magit dockerfile-mode rjsx-mode rjsx indium js-comint helm-dash flymake-solidity solidity-mode go-mode projectile ivy buffer-move dracula-theme pyvenv ace-window atom-one-dark atom-dark-theme atom-one-dark-theme nov imenu-anywhere counsel-dash rubocop mmm-jinja2 groovy-mode company meghanada ivy-gitlab gitlab mvn dired-quick-sort hydra dired+ lorem-ipsum hc-zenburn-theme zenburn-theme swiper all-the-icons-ivy org org-brain undo-tree avy f s beacon vhdl-tools company-c-headers web-mode company-tern tern-auto-complete nodejs-repl tern mmm-mode better-shell py-autopep8 intero toml-mode haskell-mode ac-haskell-process tide dired-hacks-utils yaml-mode use-package typescript tablist sudo-edit spinner solarized-theme seq restclient queue powershell pdf-tools ox-pandoc org-present org-mobile-sync multi-eshell markdown-mode magit-gh-pulls know-your-http-well key-seq json-mode jinja2-mode ivy-hydra inf-ruby ido-vertical-mode helm-projectile helm-org-rifle helm-mt helm-gitlab helm-ag hcl-mode gradle-mode golint go-eldoc go-autocomplete gist ggtags flycheck flx-ido exec-path-from-shell eshell-manual elpy dumb-jump counsel-projectile clojure-mode cl-lib-highlight ansible-doc ag)))
  '(typescript-indent-level 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -714,64 +714,80 @@ SIZE :
 
 (use-package counsel-gtags
   :ensure t
-  :defer t)
-
-(use-package irony
-  :ensure t
   :defer t
   :init
-  (add-hook 'c++-mode-hook 'load-irony-mode)
-  (add-hook 'c-mode-hook 'load-irony-mode)
-  (add-hook 'objc-mode-hook 'load-irony-mode)
-  :config
-  ;; replace the `completion-at-point' and `complete-symbol' bindings in
-  ;; irony-mode's buffers by irony-mode's function
-  (defun my-irony-mode-hook ()
-    (define-key irony-mode-map [remap completion-at-point]
-      'irony-completion-at-point-async)
-    (define-key irony-mode-map [remap complete-symbol]
-      'irony-completion-at-point-async)
-    (define-key irony-mode-map (kbd "C-c d") 'irony-get-type)
-    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-    (add-hook 'irony-mode-hook 'irony-eldoc)
-    (add-hook 'c-mode-hook 'counsel-gtags-mode)
-    (add-hook 'c++-mode-hook 'counsel-gtags-mode)
-    ;; enable use of gtags
-    (with-eval-after-load 'counsel-gtags
-      (define-key counsel-gtags-mode-map (kbd "M-.") 'counsel-gtags-find-definition)
-      (define-key counsel-gtags-mode-map (kbd "M-r") 'counsel-gtags-find-reference)
-      (define-key counsel-gtags-mode-map (kbd "M-s") 'counsel-gtags-find-symbol)
-      (define-key counsel-gtags-mode-map (kbd "M-,") 'counsel-gtags-go-backward))
-    )
-  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-  )
- 
-(use-package flycheck-irony
+  (setq tags-file-name "GTAGS"))
+
+
+;; use in place of irony mode
+(use-package ccls
   :ensure t
-  :config
-  (progn
-    (add-hook 'c-mode-hook
-              (lambda ()
-                (flycheck-mode)
-                (company-mode)
-                (use-package company-irony :ensure t
-                  :config
-                  (add-to-list 'company-backends 'company-irony))
-                (use-package company-c-headers :ensure t
-                   :config
-                   (add-to-list 'company-c-headers-path-system "/usr/include")
-                   (add-to-list 'company-backends 'company-c-headers))
-                ;; (use-package company-gtags :ensure t
-                ;;    :config
-                ;;    (add-to-list 'company-backends 'company-gtags))
-                ;; (require 'company-gtags)
-                ;; (require 'company-c-headers)
-                ;; (define-key c-mode-map  [(tab)] 'company-complete)
+  :defer t
+  :hook ((c-mode c++-mode objc-mode) .
+         (lambda () (require 'ccls) (lsp)))
+  :init
+  (use-package lsp-mode :commands lsp)
+  (use-package lsp-ui :commands lsp-ui-mode)
+  (use-package company-lsp :commands company-lsp)
+  (setq ccls-executable "/home/hitman/bin/ccls"))
+
+
+;; (use-package irony
+;;   :ensure t
+;;   :defer t
+;;   :init
+;;   (add-hook 'c++-mode-hook 'load-irony-mode)
+;;   (add-hook 'c-mode-hook 'load-irony-mode)
+;;   (add-hook 'objc-mode-hook 'load-irony-mode)
+;;   :config
+;;   ;; replace the `completion-at-point' and `complete-symbol' bindings in
+;;   ;; irony-mode's buffers by irony-mode's function
+;;   (defun my-irony-mode-hook ()
+;;     (define-key irony-mode-map [remap completion-at-point]
+;;       'irony-completion-at-point-async)
+;;     (define-key irony-mode-map [remap complete-symbol]
+;;       'irony-completion-at-point-async)
+;;     (define-key irony-mode-map (kbd "C-c d") 'irony-get-type)
+;;     (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+;;     (add-hook 'irony-mode-hook 'irony-eldoc)
+;;     (add-hook 'c-mode-hook 'counsel-gtags-mode)
+;;     (add-hook 'c++-mode-hook 'counsel-gtags-mode)
+;;     ;; enable use of gtags
+;;     (with-eval-after-load 'counsel-gtags
+;;       (define-key counsel-gtags-mode-map (kbd "M-.") 'counsel-gtags-find-definition)
+;;       (define-key counsel-gtags-mode-map (kbd "M-r") 'counsel-gtags-find-reference)
+;;       (define-key counsel-gtags-mode-map (kbd "M-s") 'counsel-gtags-find-symbol)
+;;       (define-key counsel-gtags-mode-map (kbd "M-,") 'counsel-gtags-go-backward))
+;;     )
+;;   (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+;;   )
+ 
+;; (use-package flycheck-irony
+;;   :ensure t
+;;   :config
+;;   (progn
+;;     (add-hook 'c-mode-hook
+;;               (lambda ()
+;;                 (flycheck-mode)
+;;                 (company-mode)
+;;                 (use-package company-irony :ensure t
+;;                   :config
+;;                   (add-to-list 'company-backends 'company-irony))
+;;                 (use-package company-c-headers :ensure t
+;;                    :config
+;;                    (add-to-list 'company-c-headers-path-system "/usr/include")
+;;                    (add-to-list 'company-backends 'company-c-headers))
+;;                 ;; (use-package company-gtags :ensure t
+;;                 ;;    :config
+;;                 ;;    (add-to-list 'company-backends 'company-gtags))
+;;                 ;; (require 'company-gtags)
+;;                 ;; (require 'company-c-headers)
+;;                 ;; (define-key c-mode-map  [(tab)] 'company-complete)
                 
-                ;; (push company-backends 'company-gtags)
-                ;; (push company-backends 'company-c-headers)
-                (eval-after-load 'flycheck
-                  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))))))
+;;                 ;; (push company-backends 'company-gtags)
+;;                 ;; (push company-backends 'company-c-headers)
+;;                 (eval-after-load 'flycheck
+;;                   '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))))))
 
 
 ;; ;; use indium instead of nodejs-repl
@@ -782,6 +798,8 @@ SIZE :
 ;;   (progn
 ;;     (add-hook 'js-mode-hook #'indium-interaction-mode)
 ;;     (define-key indium-interaction-mode-map (kbd "C-c C-c") 'indium-eval-region)))
+
+
 
 (defun setup-tide-mode ()
   (interactive)
